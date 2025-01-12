513d92fa3c04c7001433eaf712b9c96dccd9a422
Prasad Nikam <nikamprasad52@gmail.com>
2025-01-12 22:07:28 +0530

    api calling url changed

--- client/APIs/axiosInstance.js
+++ client/APIs/axiosInstance.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const HOST = "http://localhost";
+const HOST = "http://13.201.223.5";
 export const NodeInstance = axios.create({
 	baseURL: `${HOST}:8080/api`,
 	timeout: 9000,
